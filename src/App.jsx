a4397d76697702122d3b8aa81b9a391c45807858
Aaftab Pinjari <pinjari.aaftab@gmail.com>
2024-03-12 11:35:15 +0530

    done with setting up env variables and appwrite id generations

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-
+import conf from "./conf/conf"
 
 function App() {
-
+  // console.table([import.meta.env.VITE_APPWRITE_URL, import.meta.env.VITE_PROJECT_ID, import.meta.env.VITE_DATABASE_ID, 
+  // import.meta.env.VITE_COLLECTION_ID, import.meta.env.VITE_BUCKET_ID])
+  // console.log(conf.appwriteUrl)
 
   return (
     <>
-      <h1>Journey</h1>
+      <h1 className="text-xl">Journey</h1>
     </>
   )
 }
